b6b3b66a4a2506b85513a8f8906413a3f43c8ec7
Temitope Faro <topriddy@gmail.com>
2018-06-10 15:36:06 +0100

    Integrated basic passport twitter login and redirect to profile page.

--- app.js
+++ app.js
@@ -4,8 +4,48 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-// import our local files
+const passport = require('passport');
+const Strategy = require('passport-twitter').Strategy;
+
 const routes = require('./routes/routes');
+
+// Configure the Twitter strategy for use by Passport.
+//
+// OAuth 1.0-based strategies require a `verify` function which receives the
+// credentials (`token` and `tokenSecret`) for accessing the Twitter API on the
+// user's behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new Strategy({
+        consumerKey: process.env.CONSUMER_KEY,
+        consumerSecret: process.env.CONSUMER_SECRET,
+        callbackURL: 'http://127.0.0.1:5000/login/twitter/return'
+    },
+    function (token, tokenSecret, profile, cb) {
+        // In this example, the user's Twitter profile is supplied as the user
+        // record.  In a production-quality application, the Twitter profile should
+        // be associated with a user record in the application's database, which
+        // allows for account linking and authentication with other identity
+        // providers.
+        return cb(null, profile);
+    }));
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Twitter profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
 
 
 // create our express app
@@ -24,13 +64,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 // enable cors
 app.use(cors())
 
-// Takes the raw requests and turns them into usable properties on req.body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 // populates req.cookies with any cookies that came along with the request
 app.use(cookieParser());
 
+// Takes the raw requests and turns them into usable properties on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
 
 // static files
 app.use("/static", express.static(path.join(__dirname, "public")));
--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,18 @@ const router = express.Router();
 
 const authController = require("../controllers/authController");
 const appController = require("../controllers/appController");
+const passport = require('passport');
 
 
 // router.post("/register", authController.registerUser);
 router.get("/users", appController.getUsers);
 
 router.get("/login", appController.loginPage);
+router.get("/login/twitter",
+    passport.authenticate('twitter'));
+router.get("/login/twitter/return",
+    passport.authenticate('twitter', {failureRedirect: '/login'}),
+    appController.loginSuccessfulRedirect);
 router.get("/signup", appController.signupPage);
 
 router.post("/login", authController.loginUser);
